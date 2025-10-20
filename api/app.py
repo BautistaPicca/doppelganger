ed120269583ed7079719d61fce6587a5c41b69d4
BautistaPicca <bautistapiccarenaudo@gmail.com>
2025-10-20 14:53:30 -0300

    fix: added cors to api

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.routes.embeddings import embeddings_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(embeddings_bp, url_prefix="/embed")
 
     return app
