0a8aefe8c21f62b5a2c30e8414bba55b1de8c8c9
JoacoBal <joaquinnicolasbalbuena@gmail.com>
2025-11-16 21:41:42 -0300

    Extension de configuracion

--- ai_engine/config.py
+++ ai_engine/config.py
@@ -11,3 +11,16 @@ INDEX_ROOT = Path("face_index")
 # FAISS config
 USE_NORMALIZATION = True
 INDEX_TYPE = "FlatL2"
+
+# Configuraciones de Clasificador 
+DATA_DIR = "data"
+MODEL_DIR = "model"
+MODEL_PATH = f"{MODEL_DIR}/best_model.pth"
+
+#Tamaño de las imagenes y del batch
+IMG_SIZE = 224
+BATCH_SIZE = 32
+
+#Cantidad de epocas para entrenamiento
+EPOCHS_INITIAL = 30
+EPOCHS_FINETUNE = 30
