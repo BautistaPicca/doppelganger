dea4ea59f1827bca43f0898ace36ef30ed6d0352
JoacoBal <joaquinnicolasbalbuena@gmail.com>
2025-11-16 23:03:10 -0300

    Metodo para cargar el modelo

--- ai_engine/model/load_Model.py
+++ ai_engine/model/load_Model.py
@@ -1,2 +1,15 @@
+import torch
+from torchvision import models
+
+#Cargamos el modelo para poder utilizarlo
+def load_model(model_path, num_classes, device):
+    model = models.mobilenet_v2(weights=None)
+    in_feats = model.classifier[1].in_features
+    model.classifier[1] = torch.nn.Linear(in_feats, num_classes)
+
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.to(device)
+    model.eval()
+    return model
 
 
